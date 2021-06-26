7317f286052fc16b372628a6ce32b4d44ee6f444
Thomas Kottke <t.kottke90@gmail.com>
2021-06-26 07:27:33 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,184 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audio = new AudioContext();
+const analyser = audio.createAnalyser();
+const gainNode = audio.createGain();
+const panner = new StereoPannerNode(audio, { pan: 0 });
+const distortion = audio.createWaveShaper();
+
+const audioElement = document.querySelector('audio');
+const track = audio.createMediaElementSource(audioElement);
+
+analyser.fftSize = 4096;
+let bufferLength = analyser.frequencyBinCount;
+let dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
+
+const canvas = document.querySelector('canvas#bars');
+const context = canvas.getContext('2d');
+
+const dotCanvas = document.querySelector('canvas#dots');
+const dotContext = dotCanvas.getContext('2d');
+
+const WIDTH = canvas.width = dotCanvas.width = window.innerWidth * 0.80;
+const HEIGHT = canvas.height = dotCanvas.height = window.innerHeight * 0.60;
+
+console.table({WIDTH, HEIGHT})
+context.fillStyle = 'rgb(0, 0, 0)';
+context.clearRect(0, 0, WIDTH, HEIGHT);
+let drawVisual;
+
+const timestampMetric = document.querySelector('#timestamp');
+const bufferLengthMetric = document.querySelector('#buffer')
+
+const blob = document.querySelector('#blob');
+
+function drawBars(fbcArray) {
+  bar_count = WIDTH/7.5;
+  
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = '#fff'
+  
+  for (let i = 0; i < bar_count; i++) {
+    bar_pos = i * 7.5;
+    bar_width = 4;
+    bar_height = -(fbcArray[i]) * 0.9; // Shorten as to avoid clipping
+    
+    context.fillRect(bar_pos, canvas.height, bar_width, bar_height);
+  }
+  
+}
+
+
+function drawDots(fbcArray) {
+  bar_count = WIDTH/7.5;
+  
+  dotContext.clearRect(0, 0, dotCanvas.width, dotCanvas.height);
+
+  for (let i = 0; i < bar_count; i++) {
+    bar_pos = i * 7.5;
+    bar_width = 4;
+    // bar_height = -(fbcArray[i]);
+    bar_height = 10;
+
+    const hue = Math.ceil((i / bar_count) * 240);
+    const saturation = 50 + Math.ceil((fbcArray[i]/255) * 50)
+    let lightness = Math.ceil((fbcArray[i]/255) * 50);
+
+    if (lightness < 33) {
+      lightness = 0;
+    }
+
+
+
+    dotContext.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`
+
+    dotContext.fillRect(bar_pos, dotCanvas.height/2, bar_width, bar_height);
+  }
+}
+
+function draw() {
+  if (!audioElement.paused) {
+    requestAnimationFrame(draw);
+  }
+
+  timestampMetric.textContent = Math.floor(audioElement.currentTime);
+  bufferLengthMetric.textContent = bufferLength;
+  
+  fbcArray = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(fbcArray);
+
+  drawBars(fbcArray);
+  drawDots(fbcArray);
+}
+
+draw()
+
+function makeDistortionCurve(amount) {
+  let k = amount || 50;
+  let n_samples = 44100
+  let curve = new Float32Array(n_samples);
+  let deg = Math.PI / 180;
+  let x;
+
+  for (let i = 0; i < n_samples; i++) {
+    x = i * 2 / n_samples - 1;
+    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+  }
+
+  return curve;
+}
+
+
+distortion.curve = makeDistortionCurve(0);
+distortion.oversample = 'none';
+
+track
+  .connect(gainNode)
+  .connect(panner)
+  .connect(distortion)
+  .connect(analyser)
+  .connect(audio.destination);
+
+const playPauseBtn = document.querySelector('#play-pause');
+playPauseBtn.addEventListener('click', function() {
+
+  // check if context is in suspended state (autoplay policy)
+  if (audio.state === 'suspended') {
+      audio.resume();
+  }
+
+  // play or pause track depending on state
+  if (this.dataset.playing === 'false') {
+      audioElement.play();
+      this.dataset.playing = 'true';
+      draw();
+  } else if (this.dataset.playing === 'true') {
+      audioElement.pause();
+      this.dataset.playing = 'false';
+  }
+
+}, false);
+
+audioElement.addEventListener('ended', function() {
+  playPauseBtn.dataset.playing = 'false';
+  audio.pause();
+  audio.currentTime = 0;
+}, false);
+
+const volumeControl = document.querySelector('#volume');
+volumeControl.addEventListener('input', function() {
+  gainNode.gain.value = this.value;
+}, false);
+
+volumeControl.addEventListener('mouseup', function(event) {
+  if (event.button === 2) {
+    gainNode.gain.value = 1;
+    this.value = 1;
+  }
+}, false);
+
+const panControl = document.querySelector('#panner');
+panControl.addEventListener('input', function() {
+  panner.pan.value = this.value;
+}, false);
+
+panControl.addEventListener('mouseup', function(event) {
+  if (event.button === 2) {
+    panner.pan.value.value = 0;
+    this.value = 0;
+  }
+}, false);
+
+
+const distortionControl = document.querySelector('#distortion');
+distortionControl.addEventListener('input', function() {
+  distortion.curve = makeDistortionCurve(this.value);
+}, false);
+
+distortionControl.addEventListener('mouseup', function(event) {
+  if (event.button === 2) {
+    distortion.curve = 0;
+    this.value = 0;
+  }
+}, false);
+
